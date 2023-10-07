c153bad2d3d35c9a0bc5452682a7d6b71485250c
korolinakila <112556625+korolinakila@users.noreply.github.com>
2023-10-07 23:50:22 +0300

    func3.cpp

---
+++ func3.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include "str_easy.h"
+#include <string>
+using namespace std;
+
+string itc_cmp_str(string str1, string str2, int num){
+    string S1="";
+    if(str2==""){
+        for (int i=0; i<=itc_len(str1); i++){
+         S1+= str1[i];
+    }
+    return S1;
+    }
+
+    if(num+itc_len(str2)>itc_len(str1)){
+    for (int i=0; i!=num; i++){
+         S1+= str1[i];
+    }
+    for (int k=0; k<itc_len(str1)-num; k++){
+         S1+= str2[k];
+    }
+    return S1;
+    }
+    else{
+    int m=0;
+    for (int i=0; i!=num; i++){
+         S1+= str1[i];
+         m=i;
+    }
+    for (int k=0; k<itc_len(str2); k++){
+         S1+= str2[k];
+    }
+    for (int y=m+1; y<=(itc_len(str1)-itc_len(str2)-1); y++){
+         S1+= str1[y];
+    }
+    return S1;
+    }
+    return S1;
+}   //11
+
+int itc_find_str(string str1, string str2){
+    string S1="";
+
+    for(int i=0; i<=itc_len(str1); i++){
+        int m=i;
+        for(int f=i; f<itc_len(str2)+m; f++){
+            S1+=str1[f];
+        }
+            if(S1==str2){
+                return i;
+            }
+        S1 = "";
+
+}   //13
+return -1;
+}
+
+string itc_three_str(string str1, string str2, string str3){
+string S1="";
+string S2="";
+int m = 0;
+if(itc_find_str(str1, str2)!=-1){
+
+    for (int i=m; i<itc_find_str(str1, str2); i++){
+         S1+= str1[i];
+         m+=1;
+    }
+
+    for(int k=0; k<itc_len(str3); k++){
+        S1+=str3[k];
+        m+=1;
+    }
+    for (int g=m; g<itc_len(str1); g++){
+         S2+= str1[g];
+    }
+}
+while(itc_find_str(S2, str2)!=-1){
+    int y=m;
+       for (int i=y; i<(itc_find_str(S2, str2)+y); i++){
+         S1+= str1[i];
+         m+=1;
+    }
+
+    for(int k=0; k<itc_len(str3); k++){
+        S1+=str3[k];
+        m+=1;
+    }
+    S2="";
+    for (int g=m; g<itc_len(str1); g++){
+         S2+= str1[g];
+    }
+}
+if(itc_find_str(S2, str2)==-1){
+int y=m;
+for (int i=m; i<(itc_len(str1)+y); i++){
+         S1+= str1[i];
+
+    }
+}
+return S1;
+}
